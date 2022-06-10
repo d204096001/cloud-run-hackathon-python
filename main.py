ed91b39dcb22ffff55a8fa16329d3f74f8c75d9d
d204096001 <37644174+d204096001@users.noreply.github.com>
2022-06-10 14:58:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
-info = ""
 
 @app.route("/", methods=['GET'])
 def index():
@@ -34,8 +33,132 @@ def move():
     request.get_data()
 #     logger.info(request.json)
     json = request.json
+    
+    self_url = json['_links']['self']['href']
+    self_state = json['arena']['state'][self_url]
+    
+    def fine_nearest_corner():
+        nearest_corner_x = nearest_corner_y = 0
+        move_x = move_y = 0
+        if self_state['x'] >= json['arena']['dims'][0]/2:
+            nearest_corner_x = json['arena']['dims'][0] - 1
+        if self_state['y'] >= json['arena']['dims'][1]/2:
+            nearest_corner_y = json['arena']['dims'][1] - 1
+        move_x = nearest_corner_x - self_state['x']
+        move_y = nearest_corner_y - self_state['y']
+        return move_x, move_y
+    
+    def move(move_x, move_y):
+        if move_x > 0:
+            if self_state['direction'] == 'E':
+                return 'F'
+            elif self_state['direction'] == 'W':
+                return 'L'
+            elif self_state['direction'] == 'S':
+                return 'L'
+            elif self_state['direction'] == 'N':
+                return 'R'
+            else:
+                return 'at'
+        elif move_x < 0:
+            if self_state['direction'] == 'E':
+                return 'L'
+            elif self_state['direction'] == 'W':
+                return 'F'
+            elif self_state['direction'] == 'S':
+                return 'R'
+            elif self_state['direction'] == 'N':
+                return 'L'
+            else:
+                return 'at'
+        elif move_y > 0:
+            if self_state['direction'] == 'E':
+                return 'R'
+            elif self_state['direction'] == 'W':
+                return 'R'
+            elif self_state['direction'] == 'S':
+                return 'F'
+            elif self_state['direction'] == 'N':
+                return 'L'
+            else:
+                return 'at'
+        elif move_y < 0:
+            if self_state['direction'] == 'E':
+                return 'L'
+            elif self_state['direction'] == 'W':
+                return 'R'
+            elif self_state['direction'] == 'S':
+                return 'L'
+            elif self_state['direction'] == 'N':
+                return 'F'
+            else:
+                return 'at'
+        else:
+            return 'at'
+    
+    def fine_target_fire():
+        # update map
+        map_arr = np.zeros(json['arena']['dims'], dtype='uint8')
+        url_list = list(json['arena']['state'].keys())
+        for u in url_list:
+            x = json['arena']['state'][u]['x']
+            y = json['arena']['state'][u]['y']
+            map_arr[x, y] = 1
+        # detection
+        if self_state['x'] == 0:
+            x_near_num = map_arr[1:4, 0].sum()
+        else:
+            x_near_num = map_arr[-4:-1, 0].sum()
+        if self_state['y'] == 0:
+            y_near_num = map_arr[0, 1:4].sum()
+        else:
+            y_near_num = map_arr[0, -4:-1].sum()
+        if x_near_num >= y_near_num:
+            if self_state['x'] == 0:
+                fire_direction = 'E'
+            else:
+                fire_direction = 'W'
+        elif x_near_num < y_near_num:
+            if self_state['y'] == 0:
+                fire_direction = 'S'
+            else:
+                fire_direction = 'N'
+        # action
+        if self_state['direction'] == fire_direction:
+            return 'T'
+        elif fire_direction == 'N':
+            if self_state['direction'] == 'W':
+                return 'R'
+            else: 
+                return 'L'
+        elif fire_direction == 'E':
+            if self_state['direction'] == 'N':
+                return 'R'
+            else: 
+                return 'L'
+        elif fire_direction == 'W':
+            if self_state['direction'] == 'S':
+                return 'R'
+            else: 
+                return 'L'
+        elif fire_direction == 'S':
+            if self_state['direction'] == 'E':
+                return 'R'
+            else: 
+                return 'L'
+        else:
+            return 'T'
+    
+    move_x, move_y = fine_nearest_corner()
+    act = move(move_x, move_y)
+    if act != 'at':
+        return act
+    else: # at corner
+        act = fine_target_fire()
+        return act
+    
 #     return moves[random.randrange(len(moves))]
-    return json
+#     return json
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
